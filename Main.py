5136986f2ca85c0c16a87f58323c01f2e24af0cb
Willian Murayama <willianyos13@gmail.com>
2023-09-05 15:51:15 -0300

    Feat: implementação de quicksort

--- Main.py
+++ Main.py
@@ -25,13 +25,19 @@ def bubble(vetor,tempos):
     tempos.append((datetime.datetime.now()-inicio).total_seconds())
     print(f"Bubblesort realizado em: {datetime.datetime.now()-inicio}")
 
-def quicksort(vetor,tempos):
+def quicksort_comeco(vetor,tempos):
     inicio = datetime.datetime.now()
     Quicksort.quickSort(vetor,0,len(vetor)-1)
     tempos.append((datetime.datetime.now()-inicio).total_seconds())
     print(f"Quicksort realizado em: {datetime.datetime.now()-inicio}")
 
-dicionario = {"tempo_heap":[],"tempo_bubble":[],"tempo_quicksort":[],"quantidade_elementos":[]}
+def quicksort_meio(vetor,tempos):
+    inicio = datetime.datetime.now()
+    Quicksort.quickSort(vetor,int(np.floor(len(vetor)/2)),len(vetor)-1)
+    tempos.append((datetime.datetime.now()-inicio).total_seconds())
+    print(f"Quicksort realizado em: {datetime.datetime.now()-inicio}")
+
+dicionario = {"tempo_heap":[],"tempo_bubble":[],"tempo_quicksort_comeco":[],"tempo_quicksort_meio":[],"quantidade_elementos":[]}
 quantidade_divisao_tempo_grafico = 20
 quantidade_elemento_grafico = 30
 
@@ -52,21 +58,24 @@ for i in range(0,quantidade_divisao,1):
     dicionario["quantidade_elementos"].append(len(vetor))
     heapsort(vetor_heap,dicionario["tempo_heap"])
     bubble(vetor_bubble,dicionario["tempo_bubble"])
-    quicksort(vetor_quicksort,dicionario["tempo_quicksort"])
+    quicksort_comeco(vetor_quicksort,dicionario["tempo_quicksort_comeco"])
+    quicksort_meio(vetor_quicksort,dicionario["tempo_quicksort_meio"])
     print()
 
 tempo_maximo = []
 tempo_maximo.extend(dicionario["tempo_bubble"])
 tempo_maximo.extend(dicionario["tempo_heap"])
-tempo_maximo.extend(dicionario["tempo_quicksort"])
-
+tempo_maximo.extend(dicionario["tempo_quicksort_comeco"])
+tempo_maximo.extend(dicionario["tempo_quicksort_meio"])
+plt.figure(figsize=(8.27,5))
 # print(dicionario)
 plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_heap"],label="Heapsort")
 plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_bubble"],label="BubbleSort")
-plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_quicksort"],label="Quicksort")
+plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_quicksort_comeco"],label="Quicksort começo")
+plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_quicksort_meio"],label="Quicksort meio")
 plt.grid()
 plt.yticks(np.arange(np.min(tempo_maximo),np.max(tempo_maximo)+np.max(tempo_maximo)/quantidade_divisao_tempo_grafico,np.max(tempo_maximo)/quantidade_divisao_tempo_grafico))
-plt.xticks(dicionario["quantidade_elementos"],rotation=45)
+plt.xticks(dicionario["quantidade_elementos"],rotation=90)
 plt.xlabel("Quantidade de elementos no vetor")
 plt.ylabel("Tempo em segundos")
 plt.legend()
