332426fdd256a71753b90ce482b49a7ce94ac332
Willian Murayama <willianyos13@gmail.com>
2023-09-05 15:20:36 -0300

    Feat: quicksort

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import numpy as np
 import random 
 import matplotlib.pyplot as plt
 import Bubblesort
+import Quicksort
 
 def shuffle(vetor):
     inicio = datetime.datetime.now()
@@ -24,7 +25,13 @@ def bubble(vetor,tempos):
     tempos.append((datetime.datetime.now()-inicio).total_seconds())
     print(f"Bubblesort realizado em: {datetime.datetime.now()-inicio}")
 
-dicionario = {"tempo_heap":[],"tempo_bubble":[],"quantidade_elementos":[]}
+def quicksort(vetor,tempos):
+    inicio = datetime.datetime.now()
+    Quicksort.quickSort(vetor,0,len(vetor)-1)
+    tempos.append((datetime.datetime.now()-inicio).total_seconds())
+    print(f"Quicksort realizado em: {datetime.datetime.now()-inicio}")
+
+dicionario = {"tempo_heap":[],"tempo_bubble":[],"tempo_quicksort":[],"quantidade_elementos":[]}
 quantidade_divisao_tempo_grafico = 20
 quantidade_elemento_grafico = 30
 
@@ -37,27 +44,32 @@ step = maximo/quantidade_divisao
 for i in range(0,quantidade_divisao,1):
     vetor = np.arange(0,step*(i+1),1)
     # print(vetor)
+    print(f"Etapa: {i}/{quantidade_divisao}")
     shuffle(vetor)
     vetor_heap= vetor.copy()
     vetor_bubble= vetor.copy()
-    print(f"Etapa: {i}/{quantidade_divisao}")
+    vetor_quicksort=vetor.copy()
     dicionario["quantidade_elementos"].append(len(vetor))
     heapsort(vetor_heap,dicionario["tempo_heap"])
     bubble(vetor_bubble,dicionario["tempo_bubble"])
+    quicksort(vetor_quicksort,dicionario["tempo_quicksort"])
     print()
 
 tempo_maximo = []
 tempo_maximo.extend(dicionario["tempo_bubble"])
 tempo_maximo.extend(dicionario["tempo_heap"])
-
+tempo_maximo.extend(dicionario["tempo_quicksort"])
 
 # print(dicionario)
 plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_heap"],label="Heapsort")
 plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_bubble"],label="BubbleSort")
+plt.plot(dicionario["quantidade_elementos"],dicionario["tempo_quicksort"],label="Quicksort")
 plt.grid()
 plt.yticks(np.arange(np.min(tempo_maximo),np.max(tempo_maximo)+np.max(tempo_maximo)/quantidade_divisao_tempo_grafico,np.max(tempo_maximo)/quantidade_divisao_tempo_grafico))
 plt.xticks(dicionario["quantidade_elementos"],rotation=45)
 plt.xlabel("Quantidade de elementos no vetor")
 plt.ylabel("Tempo em segundos")
 plt.legend()
+plt.savefig(f"{datetime.datetime.now()}.png",dpi=300,format="png")
+
 plt.show()
