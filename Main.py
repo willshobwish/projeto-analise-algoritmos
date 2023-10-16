620f406c55e7ba006bdeb0a1dd997603b0252693
Willian Murayama <willianyos13@gmail.com>
2023-10-16 18:58:38 -0300

    Feat: implementação de quicksort iterativo
    Remoção de comentários

--- Main.py
+++ Main.py
@@ -26,13 +26,13 @@ def bubble(vetor,tempos):
 
 def quicksort_comeco(vetor,tempos):
     inicio = datetime.datetime.now()
-    Quicksort.quickSort(vetor,0,len(vetor)-1)
+    Quicksort.quickSortIterative(vetor,0,len(vetor)-1)
     tempos.append((datetime.datetime.now()-inicio).total_seconds())
     print(f"Quicksort realizado em: {datetime.datetime.now()-inicio}")
 
 def quicksort_meio(vetor,tempos):
     inicio = datetime.datetime.now()
-    Quicksort.quickSort(vetor,int(np.floor(len(vetor)/2)),len(vetor)-1)
+    Quicksort.quickSortIterative(vetor,int(np.floor(len(vetor)/2)),len(vetor)-1)
     tempos.append((datetime.datetime.now()-inicio).total_seconds())
     print(f"Quicksort realizado em: {datetime.datetime.now()-inicio}")
 
@@ -49,8 +49,6 @@ def grafico_individual(dicionario:dict,quantidade_elementos:list):
         plt.legend()
         name = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
         plt.savefig(f"{name} {sorts}.png",dpi=300,format="png")
-        # pandas.DataFrame(dicionario).to_csv(f"{name}{sorts}.csv",index=False)
-        # plt.show()
         plt.clf()
 
 
@@ -58,14 +56,9 @@ def grafico_todos(dicionario:dict,quantidade_elementos:list):
     tempo_maximo = []
     for sorts in dicionario.keys():
         tempo_maximo.extend(dicionario[sorts])
-    # tempo_maximo.extend(dicionario["Heapsort"])
-    # tempo_maximo.extend(dicionario["Quicksort inicio"])
     plt.figure(figsize=(8.27,6))
     for sorts in dicionario.keys():
         plt.plot(quantidade_elementos,dicionario[sorts],label=sorts)
-    # plt.plot(quantidade_elementos,dicionario["Heapsort"],label="Heapsort")
-    # plt.plot(quantidade_elementos,dicionario["Bubblesort"],label="BubbleSort")
-    # plt.plot(quantidade_elementos,dicionario["Quicksort inicio"],label="Quicksort começo")
     plt.grid()
     plt.yticks(np.arange(np.min(tempo_maximo),np.max(tempo_maximo)+np.max(tempo_maximo)/quantidade_divisao_tempo_grafico,np.max(tempo_maximo)/quantidade_divisao_tempo_grafico))
     plt.xticks(quantidade_elementos,rotation=90)
@@ -75,13 +68,12 @@ def grafico_todos(dicionario:dict,quantidade_elementos:list):
     name = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
     plt.savefig(f"{name} all.png",dpi=300,format="png")
     pandas.DataFrame(dicionario).to_csv(f"{name}.csv",index=False)
-    # plt.show()
     plt.clf()
 
 
 
 quantidade_elementos =[]
-dicionario = {"Heapsort":[],"Bubblesort":[],"Quicksort inicio":[],}
+dicionario = {"Heapsort":[],"Bubblesort":[],"Quicksort inicio":[],"Quicksort meio":[]}
 quantidade_divisao_tempo_grafico = 20
 quantidade_elemento_grafico = 30
 
@@ -100,10 +92,12 @@ for i in range(0,quantidade_divisao,1):
     vetor_heap= vetor.copy()
     vetor_bubble= vetor.copy()
     vetor_quicksort=vetor.copy()
+    vetor_quicksort_meio=vetor.copy()
     quantidade_elementos.append(len(vetor))
     heapsort(vetor_heap,dicionario["Heapsort"])
     bubble(vetor_bubble,dicionario["Bubblesort"])
     quicksort_comeco(vetor_quicksort,dicionario["Quicksort inicio"])
+    quicksort_meio(vetor_quicksort,dicionario["Quicksort meio"])
     print()
 
 grafico_individual(dicionario=dicionario,quantidade_elementos=quantidade_elementos)
